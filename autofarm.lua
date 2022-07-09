d34e9120785b0df59dafafc984da66934ed5098e
cameron <108955360+dogicanx@users.noreply.github.com>
2022-07-09 16:03:23 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -10,7 +10,22 @@ function af(bool) local players = game:GetService("Players")
             if not client.Character or not client.Character:FindFirstChild("Humanoid") then
                 repeat wait() until client.Character and client.Character:FindFirstChild("Humanoid")
             end
-     
-            client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame wait(0.1) end local function stop() end if bool == true then start() else stop() end end
+        client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame wait(0.1)
+        
+      if i == 1 then
+            wait(4)
+        else
+            wait(2)
+        end
+        
+        gold:FireServer()
+    end
+ 
+    penguin:FireServer("PenguinCharacter")
+    client.Character:Remove()
+ 
+    repeat wait() 
+    until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
+     local function stop() end if bool == true then start() else stop() end end
     
     -- to autofarm type af(true), to stop autofarm type af(false
