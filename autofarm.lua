996cb0e5486705e8ea8183d4f0833923ac8899f7
cameron <108955360+dogicanx@users.noreply.github.com>
2022-07-09 16:44:15 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,23 +1,34 @@
-function af(bool) local players = game:GetService("Players")
-    local stages = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")
-    local penguin, gold = workspace:WaitForChild("ChangeCharacter"), workspace:WaitForChild("ClaimRiverResultsGold")
-    local client = players.LocalPlayer
-        local bodyVelocity = Instance.new("BodyVelocity")
-        bodyVelocity.Velocity = Vector3.new(0, -4, 0)
-        bodyVelocity.Parent = client.Character.HumanoidRootPart
-    if  bool == true then
-        
-        for i = 1, 9 do
-            if not client.Character or not client.Character:FindFirstChild("Humanoid") then
-                repeat wait() until client.Character and client.Character:FindFirstChild("Humanoid")
-            end
+--// Services
+local players = game:GetService("Players")
+ 
+--// Workspace
+local stages = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")
+local penguin, gold = workspace:WaitForChild("ChangeCharacter"), workspace:WaitForChild("ClaimRiverResultsGold")
+ 
+--// Other
+local client = players.LocalPlayer
+ 
+--// Main
+---------
+_G.Busy = true
+while _G.Busy do
+    local bodyVelocity = Instance.new("BodyVelocity")
+    bodyVelocity.Velocity = Vector3.new(0, -4, 0)
+    bodyVelocity.Parent = client.Character.HumanoidRootPart
+    
+    for i = 1, 9 do
+        if not client.Character or not client.Character:FindFirstChild("Humanoid") then
+            repeat wait() until client.Character and client.Character:FindFirstChild("Humanoid")
+        end
+ 
         client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame wait(0.1)
-        if not bool == true then
-            local function stop()
-                client.Character.Humanoid.Health = 0
+        
+        if not _G.Busy then
+            client.Character.Humanoid.Health = 0
             exit(0)
         end
-      if i == 1 then
+        
+        if i == 1 then
             wait(4)
         else
             wait(2)
@@ -28,9 +39,9 @@ function af(bool) local players = game:GetService("Players")
  
     penguin:FireServer("PenguinCharacter")
     client.Character:Remove()
- repeat wait()
- until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
-     local function stop() end if bool == true then start() else stop() end end end
- end
+ 
+    repeat wait() 
+    until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
     
-    -- to autofarm type af(true), to stop autofarm type af(false
+end
+---------
