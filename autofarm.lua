1e817ba7bfd8f127cba6b64eb4a374c2a9b69096
cameron <108955360+dogicanx@users.noreply.github.com>
2022-07-09 14:42:53 -0400

    Create autofarm.lua

---
+++ autofarm.lua
@@ -0,0 +1,39 @@
+local players = game:GetService("Players")
+local stages = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")
+local penguin, gold = workspace:WaitForChild("ChangeCharacter"), workspace:WaitForChild("ClaimRiverResultsGold")
+local client = players.LocalPlayer
+---------
+_G.Busy = true
+while _G.Busy do
+    local bodyVelocity = Instance.new("BodyVelocity")
+    bodyVelocity.Velocity = Vector3.new(0, -4, 0)
+    bodyVelocity.Parent = client.Character.HumanoidRootPart
+    
+    for i = 1, 9 do
+        if not client.Character or not client.Character:FindFirstChild("Humanoid") then
+            repeat wait() until client.Character and client.Character:FindFirstChild("Humanoid")
+        end
+ 
+        client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame wait(0.1)
+        
+        if not _G.Busy then
+            client.Character.Humanoid.Health = 0
+            exit(0)
+        end
+        
+        if i == 1 then
+            wait(4)
+        else
+            wait(2)
+        end
+        
+        gold:FireServer()
+    end
+ 
+    penguin:FireServer("PenguinCharacter")
+    client.Character:Remove()
+ 
+    repeat wait() 
+    until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
+    
+end
