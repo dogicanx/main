c7cac30415d8a5fe1ac1ae6468462b762c67d64b
cameron <108955360+dogicanx@users.noreply.github.com>
2022-07-10 12:33:15 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -1,27 +1,41 @@
-function farm(bool)
- --// Services
-local players = game:GetService("Players")
+local AutoFarm  = false
+function Repeat1()
+    local players = game:GetService("Players")
 local stages = workspace:WaitForChild("BoatStages"):WaitForChild("NormalStages")
 local penguin, gold = workspace:WaitForChild("ChangeCharacter"), workspace:WaitForChild("ClaimRiverResultsGold")
 local client = players.LocalPlayer
     local bodyVelocity = Instance.new("BodyVelocity")
     bodyVelocity.Velocity = Vector3.new(0, -4, 0)
     bodyVelocity.Parent = client.Character.HumanoidRootPart
+    
     for i = 1, 9 do
         if not client.Character or not client.Character:FindFirstChild("Humanoid") then
             repeat wait() until client.Character and client.Character:FindFirstChild("Humanoid")
         end
+ 
         client.Character.HumanoidRootPart.CFrame = stages["CaveStage"..i].DarknessPart.CFrame wait(0.1)
-       end
+        
         if i == 1 then
             wait(4)
         else
             wait(2)
         end
+        
         gold:FireServer()
     end
-    penguin:FireServer("PenguinCharacter")
-    client.Character:Remove()
-    repeat wait() 
-    until client.Character and client.Character:FindFirstChild("HumanoidRootPart")
-local function stop() end if bool == true then start() else stop() end
+end
+end
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
+    wait(4)
+    if AutoFarm ==  true then
+        Repeat1()
+    end
+end)
+AutoFarmButtons["1-1"].Button.MouseButton1Down:connect(function()
+    if AutoFarm ==  false then
+        AutoFarm =  true
+        game.Players.LocalPlayer.Character:FindFirstChild("Head"):Destroy()
+    else
+        AutoFarm = false
+    end
+end)
